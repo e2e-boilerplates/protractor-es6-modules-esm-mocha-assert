ca88a591636441504e5c6837a8561a6f4f9fbde7
Girma Nigusse <xgirma@gmail.com>
2020-03-08 23:37:44 -0400

    update,update test url

--- spec/sandbox.spec.js
+++ spec/sandbox.spec.js
@@ -2,7 +2,7 @@ import assert from "assert";
 
 describe("Sandbox", () => {
   before(() => {
-    browser.get("https://e2e-boilerplates.github.io/sandbox/");
+    browser.get("https://e2e-boilerplate.github.io/sandbox/");
   });
 
   it("should be on Sandbox", async () => {
